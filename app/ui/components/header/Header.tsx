2c2efc75d66d08c431308f1267bb01d887e51135
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-05-06 22:21:07 +0200

    feat: show current env in header

--- app/ui/components/header/Header.tsx
+++ app/ui/components/header/Header.tsx
@@ -1,9 +1,10 @@
 import NavLinks from './nav-links';
 
 export default function Header() {
+  const env = process.env.NODE_ENV;
   return (
     <header>
-      <p>Header</p>
+      <p>Header current env === {env}</p>
       <NavLinks />
     </header>
   );
