3f0ecdda0aa4eca611e13f50435b135fa7de9313
Galya Bogdanova <galya_bogdanova@MacBook-Air-Maksim.local>
2024-05-07 12:57:09 -0700

    refactor: validation is moved to separate file

--- app/ui/components/login/Login.tsx
+++ app/ui/components/login/Login.tsx
@@ -1,17 +1,13 @@
 'use client';
 
 import React, { useState } from 'react';
+import { isValidEmail, isValidPassword } from '@/app/utils/validation';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
-
-  const isValidEmail = (mail: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(mail);
-  };
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value.trim();
@@ -23,31 +19,6 @@ export default function Login() {
     } else {
       setEmailError('');
     }
-  };
-
-  const isValidPassword = (pass: string): string => {
-    if (!pass) {
-      return 'Password is required';
-    }
-    if (pass.length < 8) {
-      return 'Password must be at least 8 characters long';
-    }
-    if (!/[A-Z]/.test(pass)) {
-      return 'Password must contain at least one uppercase letter (A-Z)';
-    }
-    if (!/[a-z]/.test(pass)) {
-      return 'Password must contain at least one lowercase letter (a-z)';
-    }
-    if (!/\d/.test(pass)) {
-      return 'Password must contain at least one digit (0-9)';
-    }
-    if (!/[!@#$%^&*]/.test(pass)) {
-      return 'Password must contain at least one special character (!@#$%^&*)';
-    }
-    if (pass !== pass.trim()) {
-      return 'Password must not contain leading or trailing whitespace';
-    }
-    return '';
   };
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
---
+++ app/utils/validation.ts
@@ -0,0 +1,28 @@
+export const isValidEmail = (mail: string) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(mail);
+};
+export const isValidPassword = (pass: string): string => {
+  if (!pass) {
+    return 'Password is required';
+  }
+  if (pass.length < 8) {
+    return 'Password must be at least 8 characters long';
+  }
+  if (!/[A-Z]/.test(pass)) {
+    return 'Password must contain at least one uppercase letter (A-Z)';
+  }
+  if (!/[a-z]/.test(pass)) {
+    return 'Password must contain at least one lowercase letter (a-z)';
+  }
+  if (!/\d/.test(pass)) {
+    return 'Password must contain at least one digit (0-9)';
+  }
+  if (!/[!@#$%^&*]/.test(pass)) {
+    return 'Password must contain at least one special character (!@#$%^&*)';
+  }
+  if (pass !== pass.trim()) {
+    return 'Password must not contain leading or trailing whitespace';
+  }
+  return '';
+};
