4868730c49fb5ebf052464ae07e1fdfb297cfabc
antonina220590 <tonya2290@gmail.com>
2024-05-09 16:48:48 +0300

    feat: add burger menu and responsive styles

--- app/ui/components/header/Header.tsx
+++ app/ui/components/header/Header.tsx
@@ -1,21 +1,51 @@
 'use client';
 
 import cl from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLinks, NavIcons } from './nav-links';
 import Logo from './Logo';
 import styles from '../../styles/components/header.module.scss';
+import style from '../../styles/components/hamburger.module.scss';
 
 export default function Header() {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(false);
+  const handleNavClose = () => {
+    if (isNavCollapsed && window.innerWidth < 1024) {
+      setIsNavCollapsed(false);
+    }
+  };
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  window.addEventListener('resize', handleNavClose);
+
   return (
-    <header className={cl(styles.header)}>
+    <header
+      className={cl(styles.header)}
+      onClick={handleNavClose}
+      role="presentation"
+    >
       <div className={cl(styles.headerWrapper)}>
-        <Logo />
-        <div className={cl(styles.headerLinksContainer)}>
-          <NavLinks />
+        <div className={cl(styles.headerContainer)}>
+          <Logo />
+          <div
+            className={
+              isNavCollapsed
+                ? styles.headerLinksContainer_open
+                : styles.headerLinksContainer
+            }
+          >
+            <NavLinks />
+          </div>
         </div>
         <div className={styles.headerIcons}>
           <NavIcons />
+        </div>
+        <div
+          className={cl(style.hamburger)}
+          onClick={handleNavCollapse}
+          role="presentation"
+        >
+          <span />
         </div>
       </div>
     </header>
