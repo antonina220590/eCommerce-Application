e6b3b5aa05e2d008415ddb210ec78ab601c0260a
Antonina Tyurina <119872378+antonina220590@users.noreply.github.com>
2024-05-14 20:25:15 +0300

    feat: add footer (#57)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import Header from './ui/components/headerComponent/Header';
-import Footer from './ui/components/footer/Footer';
+import Footer from './ui/components/footerComponent/Footer';
 
 import './ui/styles/main.scss';
 
