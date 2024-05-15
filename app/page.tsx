972bbf8ec4d3812d2f270e51cdecd650a70289bd
antonina220590 <tonya2290@gmail.com>
2024-05-15 09:05:18 +0300

    feat: add section about to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
+import clsx from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './mainPage.module.scss';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Homepage</h1>
-      <h2>test</h2>
-      <h3>test</h3>
-      <h4>test</h4>
+    <main className={clsx(styles.main)}>
+      <section className={clsx(styles.mainAbout)}>
+        <div className={clsx(styles.mainAboutBox)}>
+          <h2 className={clsx(styles.mainAboutTitle)}>
+            WELCOME TO YOUR HAPPY PLACE.
+          </h2>
+          <p className={clsx(styles.mainAboutText)}>
+            Shop brand new books at least 50% off* list prices every day.
+          </p>
+          <Link className={clsx(styles.mainAboutBtn)} href="/catalog">
+            See For Yourself
+          </Link>
+        </div>
+        <div className={clsx(styles.mainAboutImg)}>
+          <Image
+            className={clsx(styles.mainAboutImgPicture)}
+            src="/cover.webp"
+            alt="man and woman"
+            width="1200"
+            height="1250"
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+            }}
+            role="presentation"
+            sizes="100vw"
+          />
+        </div>
+      </section>
     </main>
   );
 }
