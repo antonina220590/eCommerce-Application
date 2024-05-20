5d5aa8c6305ae5ee18e882d81c215545d0d7e44d
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-05-20 02:43:45 +0200

    feat: logout logic

--- app/ui/components/headerComponent/links/iconLinks/icon-links.tsx
+++ app/ui/components/headerComponent/links/iconLinks/icon-links.tsx
@@ -1,27 +1,63 @@
 'use client';
 
+import React, { useEffect, useState, MouseEvent } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { parseCookies, destroyCookie } from 'nookies';
 import clsx from 'clsx';
 import linkIcons from './links';
 
 export default function NavIcons() {
   const pathname = usePathname();
+  const cookies = parseCookies();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const { userName } = cookies;
+
+  useEffect(() => {
+    if (userName) {
+      setIsLoggedIn(true);
+    }
+    setLoading(false);
+  }, [userName]);
+
+  const filteredLinks = linkIcons.filter((link) => {
+    if (link.name === 'Sign Up' || link.name === 'Login') {
+      return !isLoggedIn;
+    }
+    if (link.name === 'Logout') {
+      return isLoggedIn;
+    }
+    return true;
+  });
+
+  const handleClick = (e: MouseEvent<HTMLAnchorElement>, linkName: string) => {
+    switch (linkName) {
+      case 'Logout':
+        e.preventDefault();
+        Object.keys(cookies).forEach((cookieName) => {
+          destroyCookie(null, cookieName, { path: '/' });
+        });
+        window.location.href = '/';
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <>
-      {linkIcons.map((link) => {
-        return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx({
-              active: pathname === link.href,
-            })}
-          >
-            {link.image}
-          </Link>
-        );
-      })}
-    </>
+    !loading &&
+    filteredLinks.map((link) => (
+      <Link
+        key={link.name}
+        href={link.href}
+        className={clsx({
+          active: pathname === link.href,
+        })}
+        onClick={(e) => handleClick(e, link.name as string)}
+      >
+        {link.image}
+      </Link>
+    ))
   );
 }
