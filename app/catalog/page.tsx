8d82ddbf026ed74d6de79845d8bfc6085827dec8
Galya Bogdanova <galya_bogdanova@MacBook-Air-Maksim.local>
2024-05-23 16:34:06 -0700

    feat: add cards component

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,41 +1,11 @@
-// import fetchAllProducts from '@/app/utils/products/fetchAllProducts';
+// import React from 'react';
+// import Cards from '../ui/components/cards/Cards';
 
-// // Function component defined using a function declaration
-// export default async function Catalog() {
-//   let products = [];
-//   try {
-//     const response = await fetchAllProducts();
-//     products = response.results;
-//   } catch (error) {
-//     console.error('Failed to load products', error);
-//   }
-
+// function Catalog() {
 //   return (
-//     <div>
-//       <h1>Catalog</h1>
-//       <div>
-//         {products.map((product) => (
-//           <div key={product.id}>
-//             <h2>{product.name['en-US']}</h2>
-//             <ul>
-//               {Object.entries(product).map(([key, value]) => (
-//                 <li key={key}>
-//                   <strong>{key}:</strong> {JSON.stringify(value)}
-//                 </li>
-//               ))}
-//             </ul>
-//             {product.masterVariant.images &&
-//             product.masterVariant.images.length > 0 ? (
-//               <img
-//                 src={product.masterVariant.images[0].url}
-//                 alt={product.name.en}
-//               />
-//             ) : (
-//               <p>No image available</p>
-//             )}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
+//     <main>
+//       <Cards />
+//     </main>
 //   );
 // }
+// export default Catalog;
---
+++ app/ui/components/cards/Cards.tsx
@@ -0,0 +1,40 @@
+// import fetchAllProducts from '@/app/utils/products/fetchAllProducts';
+
+// export default async function Cards() {
+//   let products = [];
+//   try {
+//     const response = await fetchAllProducts();
+//     products = response.results;
+//   } catch (error) {
+//     console.error('Failed to load products', error);
+//   }
+
+//   return (
+//     <div>
+//       <h1>Catalog</h1>
+//       <div>
+//         {products.map((product) => (
+//           <div key={product.id}>
+//             <h2>{product.name['en-US']}</h2>
+//             <ul>
+//               {Object.entries(product).map(([key, value]) => (
+//                 <li key={key}>
+//                   <strong>{key}:</strong> {JSON.stringify(value)}
+//                 </li>
+//               ))}
+//             </ul>
+//             {product.masterVariant.images &&
+//             product.masterVariant.images.length > 0 ? (
+//               <img
+//                 src={product.masterVariant.images[0].url}
+//                 alt={product.name.en}
+//               />
+//             ) : (
+//               <p>No image available</p>
+//             )}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
