c29d2ef9ca09a850ac8d5045eb38e67eeebfce23
Galya Bogdanova <galya_bogdanova@MacBook-Air-Maksim.local>
2024-05-24 11:23:50 -0700

    feat: card styling - 3 in a row

--- app/ui/components/cards/Cards.tsx
+++ app/ui/components/cards/Cards.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+import style from '@/app/ui/components/cards/cards.module.scss';
 import Image from 'next/image';
 import fetchAllProducts from '@/app/utils/products/fetchAllProducts';
 import { Product } from '@/app/types/product';
@@ -16,38 +18,35 @@ export default async function Cards() {
   }
 
   return (
-    <div>
-      <div>
-        {products.map((product) => (
-          <div id={product.id}>
-            <a href={`/${product.key}`}>
-              {product.masterVariant.images &&
-              product.masterVariant.images.length > 0 ? (
-                <Image
-                  src={product.masterVariant.images[0].url}
-                  alt={product.name['en-US']}
-                  width={300}
-                  height={300}
-                  layout="responsive"
-                />
-              ) : (
-                <p>No image available</p>
-              )}
-              {product.masterVariant.prices &&
-              product.masterVariant.prices.length > 0 ? (
-                <p>${product.masterVariant.prices[0].value.centAmount / 100}</p>
-              ) : (
-                <p>Price not available</p>
-              )}
-              <h3>{product.name['en-US']}</h3>
-              <p>
-                {product.metaDescription?.['en-US'] ??
-                  'No description available'}
-              </p>
-            </a>
-          </div>
-        ))}
-      </div>
+    <div className={clsx(style.productsList)}>
+      {products.map((product) => (
+        <div id={product.id} className={clsx(style.productCard)}>
+          <a href={`/${product.key}`}>
+            {product.masterVariant.images &&
+            product.masterVariant.images.length > 0 ? (
+              <Image
+                src={product.masterVariant.images[0].url}
+                alt={product.name['en-US']}
+                width={300}
+                height={300}
+                layout="responsive"
+              />
+            ) : (
+              <p>No image available</p>
+            )}
+            {product.masterVariant.prices &&
+            product.masterVariant.prices.length > 0 ? (
+              <p>${product.masterVariant.prices[0].value.centAmount / 100}</p>
+            ) : (
+              <p>Price not available</p>
+            )}
+            <h3>{product.name['en-US']}</h3>
+            <p>
+              {product.metaDescription?.['en-US'] ?? 'No description available'}
+            </p>
+          </a>
+        </div>
+      ))}
     </div>
   );
 }
