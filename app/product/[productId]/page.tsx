835ef3c46beae196b09e8016a917f559626b1ac6
antonina220590 <tonya2290@gmail.com>
2024-05-27 17:20:21 +0300

    feat: add image, title and description to the product page

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import React from 'react';
 import ProductDetails from '@/app/ui/components/productDetails/productDetails';
+import style from '@/app/ui/components/productDetails/productDetails.module.scss';
 
 export default function Product() {
   return (
-    <main>
+    <main className={style.productMain}>
       <ProductDetails />
     </main>
   );
--- app/ui/components/productDetails/productDetails.tsx
+++ app/ui/components/productDetails/productDetails.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { Product } from '@commercetools/platform-sdk';
+import style from '@/app/ui/components/productDetails/productDetails.module.scss';
 import fetchProductById from '@/app/utils/product/fetchProductById';
 import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import clsx from 'clsx';
 
 function ProductDetails() {
   const [product, setProduct] = useState<Product | null>(null);
@@ -19,8 +22,46 @@ function ProductDetails() {
     };
     fetchProduct().catch(console.error);
   }, []);
-  console.log(product);
+  console.log(product?.masterData.current.name);
 
-  return <div>{product && <h1>{product.id}</h1>}</div>;
+  return (
+    <div>
+      {product ? (
+        <div className={clsx(style.productPageWrapper)}>
+          <div className={clsx(style.productInsideContainer)}>
+            <div className={clsx(style.productLeftSideContainer)}>
+              <div className={clsx(style.imgBox)}>
+                {product.masterData.current.masterVariant.images &&
+                product.masterData.current.masterVariant.images.length > 0 ? (
+                  <Image
+                    src={product.masterData.current.masterVariant.images[0].url}
+                    alt={product.masterData.current.name['en-US']}
+                    fill
+                    sizes="max-width: 800px"
+                    style={{ objectFit: 'scale-down' }}
+                  />
+                ) : (
+                  <p>No image available</p>
+                )}
+              </div>
+            </div>
+            <div className={clsx(style.productRightSideContainer)}>
+              <h3 className={clsx(style.productTitle)}>
+                {product.masterData.current.name['en-US']}
+              </h3>
+              <div className={clsx(style.productFullDescription)}>
+                <p className={clsx(style.productDescriptionText)}>
+                  {product.masterData.current.description?.['en-US'] ??
+                    'No description available'}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <p> ...loading... </p>
+      )}
+    </div>
+  );
 }
 export default ProductDetails;
