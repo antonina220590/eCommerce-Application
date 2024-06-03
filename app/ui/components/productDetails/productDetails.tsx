c1c1b58275940c9ecae63d265653acd4c0cf53fe
antonina220590 <tonya2290@gmail.com>
2024-06-03 13:19:47 +0300

    feat: add enlarged slider

--- app/ui/components/productDetails/productDetails.tsx
+++ app/ui/components/productDetails/productDetails.tsx
@@ -13,10 +13,13 @@ import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import Slider from '@/app/ui/components/slider/slider';
 import Image from 'next/image';
+import SliderBig from '@/app/ui/components/slider/sliderEnlarged';
+import Modal from '../modal/modal';
 
 function ProductDetails() {
   const [product, setProduct] = useState<Product | null>(null);
   const [open, setOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
 
   const toggleTextFullVisibility = () => {
     setOpen(!open);
@@ -39,18 +42,18 @@ function ProductDetails() {
   return (
     <div>
       {product ? (
-        <div className={clsx(style.productPageWrapper)}>
-          <div className={clsx(style.productInsideContainer)}>
-            <div className={clsx(style.productLeftSideContainer)}>
+        <>
+          <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+            <div>
               {product.masterData.current.masterVariant.images &&
               product.masterData.current.masterVariant.images.length > 1 ? (
                 <div className={clsx(style.sliderBox)}>
                   <div className={clsx(style.sliderInnerBox)}>
-                    <Slider />
+                    <SliderBig />
                   </div>
                 </div>
               ) : (
-                <div className={clsx(style.imgBox)}>
+                <div className={clsx(style.imgBoxSlider)}>
                   {product.masterData.current.masterVariant.images &&
                   product.masterData.current.masterVariant.images.length > 0 ? (
                     <Image
@@ -67,60 +70,93 @@ function ProductDetails() {
                   )}
                 </div>
               )}
-              <div className={clsx(style.productDetailsPrice)}>
-                {price ? (
-                  <div className={clsx(style.productPrice)}>
-                    {discountedPrice ? (
-                      <>
-                        <span className={clsx(style.fullPrice)}>
-                          ${originalPrice / 100}
-                        </span>
-                        <span className={clsx(styles.discountedPrice)}>
-                          ${discountedPrice / 100}
-                        </span>
-                      </>
-                    ) : (
-                      <span>${originalPrice / 100}</span>
-                    )}
+            </div>
+          </Modal>
+          <div className={clsx(style.productPageWrapper)}>
+            <div className={clsx(style.productInsideContainer)}>
+              <div className={clsx(style.productLeftSideContainer)}>
+                {product.masterData.current.masterVariant.images &&
+                product.masterData.current.masterVariant.images.length > 1 ? (
+                  <div className={clsx(style.sliderBox)}>
+                    <div className={clsx(style.sliderInnerBox)}>
+                      <Slider />
+                    </div>
                   </div>
                 ) : (
-                  <div>Price not available</div>
+                  <div className={clsx(style.imgBox)}>
+                    {product.masterData.current.masterVariant.images &&
+                    product.masterData.current.masterVariant.images.length >
+                      0 ? (
+                      <Image
+                        src={
+                          product.masterData.current.masterVariant.images[0].url
+                        }
+                        alt={product.masterData.current.name['en-US']}
+                        fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        style={{ objectFit: 'scale-down' }}
+                        onClick={() => setModalOpen(true)}
+                      />
+                    ) : (
+                      <p>No image available</p>
+                    )}
+                  </div>
                 )}
+                <div className={clsx(style.productDetailsPrice)}>
+                  {price ? (
+                    <div className={clsx(style.productPrice)}>
+                      {discountedPrice ? (
+                        <>
+                          <span className={clsx(style.fullPrice)}>
+                            ${originalPrice / 100}
+                          </span>
+                          <span className={clsx(styles.discountedPrice)}>
+                            ${discountedPrice / 100}
+                          </span>
+                        </>
+                      ) : (
+                        <span>${originalPrice / 100}</span>
+                      )}
+                    </div>
+                  ) : (
+                    <div>Price not available</div>
+                  )}
+                </div>
               </div>
-            </div>
-            <div className={clsx(style.productRightSideContainer)}>
-              <h3 className={clsx(style.productTitle)}>
-                {product.masterData.current.name['en-US']}
-              </h3>
-              <div
-                className={clsx(
-                  !open
-                    ? style.productFullDescription
-                    : style.productFullDescription_open
-                )}
-              >
-                <p className={clsx(style.productDescriptionText)}>
-                  {product.masterData.current.description?.['en-US'] ??
-                    'No description available'}
-                </p>
+              <div className={clsx(style.productRightSideContainer)}>
+                <h3 className={clsx(style.productTitle)}>
+                  {product.masterData.current.name['en-US']}
+                </h3>
                 <div
                   className={clsx(
                     !open
-                      ? style.productFullDescriptionBottom
-                      : style.productFullDescriptionBottom_open
+                      ? style.productFullDescription
+                      : style.productFullDescription_open
                   )}
-                />
+                >
+                  <p className={clsx(style.productDescriptionText)}>
+                    {product.masterData.current.description?.['en-US'] ??
+                      'No description available'}
+                  </p>
+                  <div
+                    className={clsx(
+                      !open
+                        ? style.productFullDescriptionBottom
+                        : style.productFullDescriptionBottom_open
+                    )}
+                  />
+                </div>
+                <button
+                  className={clsx(style.productDescriptionButton)}
+                  type="button"
+                  onClick={toggleTextFullVisibility}
+                >
+                  {!open ? 'Read more...' : 'Read less...'}
+                </button>
               </div>
-              <button
-                className={clsx(style.productDescriptionButton)}
-                type="button"
-                onClick={toggleTextFullVisibility}
-              >
-                {!open ? 'Read more...' : 'Read less...'}
-              </button>
             </div>
           </div>
-        </div>
+        </>
       ) : (
         <p> ...loading... </p>
       )}
