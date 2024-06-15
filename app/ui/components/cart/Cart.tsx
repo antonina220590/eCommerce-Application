85739ee112e9883640aec4411c37686d876e494e
Antonina Tyurina <119872378+antonina220590@users.noreply.github.com>
2024-06-15 19:54:36 +0300

    Rss ecomm 4 10 fetch and display cart items using commercetools api (#140)
    * feat: add cart items to the basket page
    
    * feat: add responsive styles
    
    * feat: add delete button

--- app/ui/components/cart/Cart.tsx
+++ app/ui/components/cart/Cart.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import clsx from 'clsx';
-import style from '@/app/ui/components/cards/cards.module.scss';
 import React, { useEffect, useState } from 'react';
+// import  useCallback from 'react';
 import fetchProductsFromCart from '@/app/utils/cart/fetchProductsFromCart';
 import { LineItem } from '@commercetools/platform-sdk';
+import styles from '@/app/ui/components/cart/cart.module.scss';
+import Image from 'next/image';
 
 export default function Cart() {
   const [products, setProducts] = useState<LineItem[] | null>(null);
+  // const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -19,16 +22,102 @@ export default function Cart() {
     fetchProducts().catch(console.error);
   }, []);
 
+  // const handleIncrease = useCallback(() => {
+  // Это на всякий случай. Вдруг тебе пригодится
+
+  // }, []);
+
+  // const handleDecrease = useCallback(() => {
+  // Это на всякий случай. Вдруг тебе пригодится
+
+  // }, []);
+
   return (
-    <div>
+    <div className={clsx(styles.basketWrapper)}>
+      <h3 className={clsx(styles.basketTitle)}>Shopping Cart</h3>
       {products ? (
-        <div className={clsx(style.productsList)}>
+        <div className={clsx(styles.cartList)}>
+          <div className={clsx(styles.upperBox)}>
+            <div className={clsx(styles.cartProduct)}>Product</div>
+            <div className={clsx(styles.cartPriceInitial, styles.cartColums)}>
+              Price
+            </div>
+            <div className={clsx(styles.cartQuantity, styles.cartColums)}>
+              Quantity
+            </div>
+            <div className={clsx(styles.cartPriceTotal, styles.cartColumsEnd)}>
+              Total
+            </div>
+          </div>
           {products.map((product) => (
-            <p key={product.id}> {product.name['en-US']}</p>
+            <div className={clsx(styles.cartContainer)} key={product.id}>
+              <div className={clsx(styles.productBox)}>
+                <div className={clsx(styles.imgCartBox)}>
+                  {product.variant.images &&
+                  product.variant.images.length > 0 ? (
+                    <Image
+                      src={product.variant.images[0].url}
+                      alt={product.name['en-US']}
+                      priority
+                      fill
+                      sizes="max-width: 800px"
+                      style={{ objectFit: 'scale-down' }}
+                    />
+                  ) : (
+                    <p>No image available</p>
+                  )}
+                </div>
+                <div className={clsx(styles.nameBox)}>
+                  <span className={clsx(styles.productName, styles.cartPrices)}>
+                    {product.name['en-US']}
+                  </span>
+                  <button className={clsx(styles.deleteBtn)} type="button">
+                    Delete
+                  </button>
+                </div>
+              </div>
+              <div className={clsx(styles.infoPrice, styles.cartPrices)}>
+                {product.price ? (
+                  <div className={clsx(styles.productPrice)}>
+                    <span className={clsx(styles.fullPrice)}>
+                      ${(product?.price?.value?.centAmount || 0) / 100}
+                    </span>
+                  </div>
+                ) : (
+                  <div>Price not available</div>
+                )}
+              </div>
+              <div className={clsx(styles.infoQuantity)}>
+                <div className={clsx(styles.quantityBox)}>
+                  <button
+                    type="button"
+                    className={clsx(styles.qtyBtn, styles.cartPrices)}
+                    // onClick={handleDecrease}
+                  >
+                    -
+                  </button>
+                  <div className={clsx(styles.cartPrices)}>{1}</div>
+                  <button
+                    type="button"
+                    className={clsx(styles.qtyBtn, styles.cartPrices)}
+                    // onClick={handleIncrease}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              {product.price ? (
+                <div className={clsx(styles.infoTotal, styles.cartPrices)}>
+                  ${(product?.totalPrice?.centAmount || 0) / 100}
+                </div>
+              ) : (
+                <div>Price not available</div>
+              )}
+            </div>
           ))}
         </div>
       ) : (
-        <p> ...loading... </p>
+        <p>...loading</p>
       )}
     </div>
   );
