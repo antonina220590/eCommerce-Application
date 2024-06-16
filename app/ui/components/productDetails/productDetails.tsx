9fe6d5cadb6b0b0361d63675ec5c1fce97b67d4b
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-06-16 16:24:34 +0200

    feat: add and remove from card for product page

--- app/ui/components/productDetails/productDetails.tsx
+++ app/ui/components/productDetails/productDetails.tsx
@@ -5,21 +5,28 @@ import 'swiper/scss/free-mode';
 import 'swiper/scss/navigation';
 import 'swiper/scss/thumbs';
 
-import { Product } from '@commercetools/platform-sdk';
+import { Product, LineItem } from '@commercetools/platform-sdk';
 import style from '@/app/ui/components/productDetails/productDetails.module.scss';
 import styles from '@/app/ui/components/cards/cards.module.scss';
 import fetchProductById from '@/app/utils/product/fetchProductById';
+import fetchProductsFromCart from '@/app/utils/cart/fetchProductsFromCart';
+import handleAddToCart from '@/app/utils/cart/handleAddToCart';
+import handleRemoveFromCart from '@/app/utils/cart/handleRemoveFromCart';
 import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import Slider from '@/app/ui/components/slider/slider';
 import Image from 'next/image';
 import SliderBig from '@/app/ui/components/slider/sliderEnlarged';
 import Modal from '../modal/modal';
+import Spinner from '../../../../public/spinner.svg';
 
 function ProductDetails() {
   const [product, setProduct] = useState<Product | null>(null);
+  const [cartProducts, setCartProducts] = useState<LineItem[] | null>(null);
   const [open, setOpen] = useState(false);
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [inCart, setInCart] = useState<boolean | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState<Record<string, boolean>>({});
 
   const toggleTextFullVisibility = () => {
     setOpen(!open);
@@ -35,9 +42,64 @@ function ProductDetails() {
     fetchProduct().catch(console.error);
   }, []);
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const fetched = await fetchProductsFromCart();
+      console.log('fetched?.cartData --> ', fetched?.cartData);
+      setCartProducts(fetched?.cartData?.lineItems);
+    };
+    const currentProductId = product?.id;
+    const isCurrentProductInCart = cartProducts?.some(
+      (productInCart) => productInCart.productId === currentProductId
+    );
+    setInCart(isCurrentProductInCart);
+
+    console.log('currentProductId --> ', currentProductId);
+
+    fetchProducts().catch(console.error);
+  }, [product]);
+
+  const addToCart = async (productId: string) => {
+    try {
+      setIsLoading((prev) => ({ ...prev, [productId]: true }));
+      const result = await handleAddToCart(productId);
+      console.log('Product added to cart:', result);
+      setIsLoading((prev) => ({ ...prev, [productId]: false }));
+      if (result.success) {
+        const updCartProducts = async () => {
+          const fetched = await fetchProductsFromCart();
+          console.log('fetched?.cartData --> ', fetched?.cartData);
+          setCartProducts(fetched?.cartData?.lineItems);
+        };
+        updCartProducts().catch(console.error);
+        setInCart(true);
+      }
+    } catch (error) {
+      console.error('Error adding product to cart:', error);
+      setIsLoading((prev) => ({ ...prev, [productId]: false }));
+    }
+  };
+
+  const handleRemoveProduct = async (id: string) => {
+    setIsLoading((prev) => ({ ...prev, [id]: true }));
+    console.log(id);
+    const currentLineItem = cartProducts?.find(
+      (productInCart) => productInCart.productId === id
+    );
+    const lineItemId = currentLineItem?.id as string;
+
+    const cartUpdate = await handleRemoveFromCart(lineItemId);
+    if (cartUpdate.success) {
+      setInCart(false);
+    }
+    setIsLoading((prev) => ({ ...prev, [id]: false }));
+  };
+
   const price = product?.masterData.current.masterVariant.prices?.[0];
   const discountedPrice = price?.discounted?.value?.centAmount;
   const originalPrice = price?.value?.centAmount || 0;
+
+  console.log('product', product);
 
   return (
     <div>
@@ -122,6 +184,31 @@ function ProductDetails() {
                     <div>Price not available</div>
                   )}
                 </div>
+                {inCart ? (
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveProduct(product.id)}
+                    style={
+                      isLoading[product.id]
+                        ? { backgroundColor: '#1B3764', color: '#FFCA42' }
+                        : {}
+                    }
+                  >
+                    Remove from Cart {isLoading[product.id] && <Spinner />}
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => addToCart(product.id)}
+                    style={
+                      isLoading[product.id]
+                        ? { backgroundColor: '#1B3764', color: '#FFCA42' }
+                        : {}
+                    }
+                    type="button"
+                  >
+                    Add to Card {isLoading[product.id] && <Spinner />}
+                  </button>
+                )}
               </div>
               <div className={clsx(style.productRightSideContainer)}>
                 <h3 className={clsx(style.productTitle)}>
